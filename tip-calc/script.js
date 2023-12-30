0320691cf1a5e86bd481124b0e2f54841f87cbf0
Aaron Varga <147112718+aaronthenihilist@users.noreply.github.com>
2023-12-30 13:51:06 -0500

    tip calc playground project

---
+++ tip-calc/script.js
@@ -0,0 +1,45 @@
+// targeting calculation button element for later event listener
+let calcBtn = document.querySelector("#calc-btn");
+
+// initiate event listener for calculation button
+calcBtn.addEventListener("click", function (e) {
+  // check for empty input fields
+  if (
+    document.querySelector("#bill").value == "" ||
+    document.querySelector("#tip").value == ""
+  ) {
+    // if empty, alert user to enter values
+    alert("Please enter both a bill & tip amount");
+    return;
+  } else {
+    // run calculation if both fields are filled
+    let billInput = document.querySelector("#bill");
+    let bill = billInput.value;
+
+    let billValue = document.querySelector("#bill-value");
+    billValue.innerHTML = "$" + bill;
+
+    let tipInput = document.querySelector("#tip");
+    let tip = tipInput.value;
+    let tipValue = document.querySelector("#tip-value");
+    tipValue.innerHTML = tip.replace(".", "") * 1 + "%";
+
+    let totalTip = bill * tip;
+
+    let totalBill = Number(bill) + Number(totalTip);
+    let totalBillValue = document.querySelector("#total-value");
+    totalBillValue.innerHTML = "$" + totalBill;
+
+    let billCalcForm = document.querySelector("#bill-calc-form");
+    billCalcForm.style.display = "none";
+
+    // create receipt element
+    let receipt = document.querySelector("#receipt");
+    receipt.style.display = "block";
+
+    let successMsg = document.querySelector(".success-msg");
+    successMsg.style.display = "block";
+
+    e.preventDefault();
+  }
+});
