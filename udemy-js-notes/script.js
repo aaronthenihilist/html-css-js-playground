8b51b3bfc93bb8d5a2fb469b0ab80852534f021f
Aaron Varga <147112718+aaronthenihilist@users.noreply.github.com>
2023-12-30 19:41:15 -0500

    arrow func notes

--- udemy-js-notes/script.js
+++ udemy-js-notes/script.js
@@ -1,4 +1,17 @@
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
 // arrow function
 const calcAge3 = (birthYear) => 2037 - birthYear;
-const age3 = calcAge3(1991);
+const age3 = calcAge3(1989);
 console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1989, "Aaron"));
